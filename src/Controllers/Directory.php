8d8a240784c0eb271a7721305c69d849cf7ae64d
Alex <alexsmith11nd@gmail.com>
2021-06-15 22:54:54 +0100

    added other sub classes

---
+++ src/Controllers/Directory.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Controllers;
+
+use DateTimeInterface;
+use src\DirectoryInterface;
+
+class Directory implements DirectoryInterface
+{
+
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function setName($name)
+    {
+        // TODO: Implement setName() method.
+    }
+
+    public function getCreatedTime()
+    {
+        // TODO: Implement getCreatedTime() method.
+    }
+
+    public function setCreatedTime(DateTimeInterface $created)
+    {
+        // TODO: Implement setCreatedTime() method.
+    }
+
+    public function getPath()
+    {
+        // TODO: Implement getPath() method.
+    }
+
+    public function setPath($path)
+    {
+        // TODO: Implement setPath() method.
+    }
+}
